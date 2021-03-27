b250c10a714c6252fa9068a3f38d18e3fc284866
FireworkPenguin <anderssonstenes@cmail.carleton.ca>
2021-03-27 19:33:49 -0400

    Added fragment display system and Refactored

--- src/clientHost/elect-client/modules/utilityFunctions.js
+++ src/clientHost/elect-client/modules/utilityFunctions.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 // Function that returns text after the html escape characrers have been replaced with html codes
 //
 // List of all escape characters (in case we want to add more): https://www.werockyourweb.com/html-escape-characters/
@@ -29,3 +31,45 @@ exports.passwordCheckDebug = (password)=>{
     return password == debugPassword
 
 }
+
+
+function fragmentCompletionFunction(filepath, data){
+
+    let test = {test:1}
+
+    if (data == null || filepath == "")
+        return ""
+
+    let text = ""
+    try {
+        text = fs.readFileSync(filepath).toString()
+    } catch (error) {
+        console.log(0)
+        return ""
+    }
+    console.log(4)
+
+    //return "x"
+
+    for (element of Object.keys(data)){
+
+        text = text.replaceAll('${'+element+'}', data[element]);
+        console.log(test[element])
+
+    }
+
+    return text;
+
+} 
+
+function fragmentStreamlined(filename, data){
+
+    if (data == null || filename == "")
+        return ""
+    
+    return fragmentCompletionFunction("./UI/HTML/fragments/" + filename, data)
+
+}
+
+exports.fragmentCompletionFunction = fragmentCompletionFunction;
+exports.fragmentStreamlined = fragmentStreamlined;
