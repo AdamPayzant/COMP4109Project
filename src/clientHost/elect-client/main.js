b04257a138dbc50a2f34a4348a858e492255c7c5
FireworkPenguin <anderssonstenes@cmail.carleton.ca>
2021-03-27 19:33:49 -0400

    Added fragment display system and Refactored

--- src/clientHost/elect-client/main.js
+++ src/clientHost/elect-client/main.js
@@ -1,25 +1,25 @@
 /*##################################*\
   Required Libraries
 \*##################################*/
-const {ipcMain, app, BrowserWindow } = require('electron');
-const {sanatizeText, passwordCheckDebug} = require('./modules/utilityFunctions.js');
+const {ipcMain, app, BrowserWindow, clipboard} = require('electron');
+const {sanatizeText, passwordCheckDebug, fragmentStreamlined} = require('./modules/utilityFunctions.js');
 const {chatHistory} = require('./modules/chatHistoryClass.js')
 const {clientCommunication} = require('./modules/clientCommuniction.js');
 const {networkInformation} = require('./modules/networkInformation.js');
-
-const fs = require('fs');
 
 
 /*##################################*\
   Internal data
 \*##################################*/
+let userData = {key:"12345678", name:"Bob"}
+let hostConnectionData = new networkInformation("127.0.0.1", "9090", "user")
+let otherUser = {key:"12345678", name:"Alice", IP:"",status:""}
 
-let UIView = null 
-let connectionData = new networkInformation("127.0.0.1", "9090", "user")
+
+let UIView = null
 let outbound = new clientCommunication();
-outbound.establishConnection(connectionData)
 let chat = new chatHistory(101013731);
-
+outbound.establishConnection(hostConnectionData)
 
 /*##################################*\
   Basic Electron Startup
@@ -62,7 +62,7 @@ app.on('activate', () => {
 \*##################################*/
 
 ipcMain.on('chatSent', (event, chatText)=>{
-    SendText(JSON.parse(chatText))
+  SendText(chatText)
 })
 
 ipcMain.on('deleteMSG', (event, msgID)=>{
@@ -86,6 +86,17 @@ ipcMain.on('loginAttempt',(event, loginData)=>{
 
 })
 
+ipcMain.on('copyMessageText', (event, msgID)=>{
+  clipboard.writeText(chat.messages.filter((e)=>{return e.order == msgID})[0].messageText)
+})
+
+ipcMain.on('copyMessageJSON', (event, msgID)=>{
+  clipboard.writeText(JSON.stringify(chat.messages.filter((e)=>{return e.order == msgID})[0]))
+})
+
+ipcMain.on('exportCoversationToJSON', ()=>{
+
+})
 
 /*##################################*\
   Internal Functions
@@ -97,6 +108,10 @@ ipcMain.on('moveTo', (event, destination)=>{
 
 ipcMain.on('fetch', (event, obj)=>{
   event.returnValue = fetchData(obj)
+})
+
+ipcMain.on('renderFrament', (event, fragmentName)=>{
+  event.returnValue = fragmentRouter(fragmentName)
 })
 
 
@@ -124,21 +139,40 @@ function changeView(destination){
     default:
       changeView('login')
 }}
-
 function fetchData(requestOBJ){
 
   switch(requestOBJ){
 
     case 'connectionData':
-      return connectionData
+      return hostConnectionData
 
     case 'activeChat':
       return chat
 
     case 'connection':
       return 2
-}}
 
+    case 'otherClientName':
+      return otherUser.name || "U. N. Owen"
+
+}}
+function fragmentRouter(fragmentName){
+
+  switch(fragmentName){
+
+    case 'userData':
+      return fragmentStreamlined("userData.html", userData)
+
+    case 'otherUserData':
+      return fragmentStreamlined("otherUserData.html", otherUser)
+
+    case 'hostData':
+      return fragmentStreamlined("hostData.html", {IP:"Bob", status:"Alive"})
+
+  }
+
+  return ""
+}
 
 /*##################################*\
   Copy of the Functions Found in host.go
@@ -183,35 +217,28 @@ function GetConversation() {
 }
 
 /* Host connection events */
-function connectToHost(loginText){
-  
-  let attemptData = JSON.parse(JSON.parse(loginText).payload)
-  //console.log("Login Sent: " + JSON.stringify(attemptData))
-  
-  //Update local connection data
-  connectionData.network =  attemptData.network || connectionData.network
-  connectionData.port =     attemptData.port ||  connectionData.port
-  connectionData.username = attemptData.username ||  connectionData.username
 
-  //This is for debugging purposes (Switch to actual login proceedure)
-  if (passwordCheckDebug(attemptData.password)){
-    return 0
-  }
+/*##################################*\
+  Functions listed in diagram 4 in purposal
+\*##################################*/
 
-  return 1
-
-  /**
-   * Two stages of this process (impliment latter):
-   * 
-   *  1. Make sure that the client can connect
-   * 
-   *  2. Attempt provide credentials
-   * 
-   *  Inform the User of the success of these two steps
-   * 
-   */
+function registerWithHost(){
 
 }
+
+function updateIPaddress(){
+
+}
+
+function updateKey(){
+
+}
+
+function getUserIP(){
+
+}
+
+/* Extra Fucntion related to the others */
 function dissconnectFromHost(){
 
 }
