4fd9f0a8db9f666358b095eb871324fef53b464e
FireworkPenguin <anderssonstenes@cmail.carleton.ca>
2021-04-05 11:29:48 -0400

    Added Chat Request Form (all forms can change as needed)

--- src/clientHost/elect-client/modules/networkInformation.js
+++ src/clientHost/elect-client/modules/networkInformation.js
@@ -1,7 +1,7 @@
 exports.networkInformation = class networkInformation{
 
     constructor(n, p, u){
-        this.network = n || "127.0.0.1"; 
+        this.ip = n || "127.0.0.1"; 
         this.port = p || "9090"; 
         this.username = u || "user";
     }
